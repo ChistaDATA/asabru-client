fc1687f46374748d257b6158bacc892c0708e2f2
josephabraham <joseph.abraham@chistadata.com>
2024-04-03 12:33:20 +0530

    feat: updated request and response to get authenticated

--- src/Client.cpp
+++ src/Client.cpp
@@ -2,8 +2,8 @@
 // Created by JOSEPH ABRAHAM on 16/03/24.
 //
 
-#include <string.h>         // contains helper functions for string operations
-#include <stdio.h>          // contains declarations and functions to help with input output operations
+#include <cstring>         // contains helper functions for string operations
+#include <cstdio>          // contains declarations and functions to help with input output operations
 #include <iostream>         // contains declarations and functions to help with input output operations
 #include <fstream>          // predefines a set or operations for handling files related to input and output.
 
@@ -15,30 +15,17 @@ int main(int argc, char **argv ) {
 
     const std::string username = argv[1];
     const std::string password = argv[2];
-    int port = std::atoi(argv[3]); // Convert port from string to int
+    const std::string  host = argv[3];
+    int port = std::atoi(argv[4]);
 
-    if (argc != 4) {
+    if (argc != 5) {
         printf("Usage: %s <file-name> <server-name> <port> \n\n", argv[0]);
         return -1;
     }
 
-  // Get params from arguments
-//  char *file_name = argv[1];
-//  char *server_name = argv[2];
-//  int port = atoi(argv[3]);
-
-//    char *username = argv[1];
-//    char *password = argv[2];
-//    char *host = argv[3];
-//    int port = atoi(argv[4]);
-
-   //todo: Get Authorised here
-   //
-
-        printf("Welcome! You can exit by pressing Ctrl+C at any time...\n");
-
-    if(AuthenticateUser(username,password,port))
+    if(AuthenticateUser(username,password,host,port))
     {
+        printf("Welcome! You can exit by pressing Ctrl+C at any time...\n");
         if (argc > 1 && std::string(argv[1]) == "-d") {
           // By default, readline does filename completion. With -d, we disable this
           // by asking readline to just insert the TAB character itself.
@@ -65,21 +52,23 @@ int main(int argc, char **argv ) {
   //return UploadFile(file_name,server_name,port);
 }
 
-bool AuthenticateUser(std::string username,std::string password,int port )
+bool AuthenticateUser(std::string username,std::string password,std::string host ,int port )
 {
     // Resolve the Handler class
     CommandDispatcher::RegisterCommand<BaseComputationCommand>("HTTPBasicLoginRequestCommand");
-    //auto handler = CommandDispatcher::GetCommand<BaseComputationCommand>("HTTPBasicLoginRequestCommand");
     ComputationContext context ;
     std::string request = "TestRequest";
     context.Put("username",username);
     context.Put("password",password);
+    context.Put("host",host);
     context.Put("port",port);
     context.Put("request", request);
 
     CommandDispatcher::Dispatch("HTTPBasicLoginRequestCommand", &context);
     std::string response = std::any_cast<std::string>(context.Get("response"));
-    std::cout << response << std::endl;
+    std::string token =std::any_cast<std::string>(context.Get("token"));
+    // std::cout << response << std::endl;
+    //std::cout << "token"<< token << std::endl;
 
     return true;
 }
